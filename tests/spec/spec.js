67a7b150c02ccefbb5563bf763db255818c1a3bb
miya0001 <miya@wpist.me>
2016-03-04 10:00:24 +0900

    revert changes in tests

--- tests/spec/spec.js
+++ tests/spec/spec.js
@@ -41,46 +41,46 @@ describe( 'have-posts directive', function() {
 		expect( $rootScope.apiRoot ).toEqual( 'http://example.jp' );
 	} ) );
 
-	// it( '<have-posts> should be div', inject( function( $rootScope, $compile ) {
-	// 	var element = $compile( '<have-posts api-root="http://example.com" post-type="posts">Post type is {{ postType }}</div>' )( $rootScope );
-	// 	$rootScope.$digest();
-	// 	$httpBackend.flush();
-	// 	expect( element.prop( 'tagName' ) ).toEqual( 'DIV' );
-	// } ) );
+	it( '<have-posts> should be div', inject( function( $rootScope, $compile ) {
+		var element = $compile( '<have-posts api-root="http://example.com" post-type="posts">Post type is {{ postType }}</div>' )( $rootScope );
+		$rootScope.$digest();
+		$httpBackend.flush();
+		expect( element.prop( 'tagName' ) ).toEqual( 'DIV' );
+	} ) );
 
-	// it( 'Text should be "Post type is posts"', inject( function( $rootScope, $compile ) {
-	// 	var element = $compile( '<have-posts api-root="http://example.com" post-type="posts">Post type is {{ postType }}</div>' )( $rootScope );
-	// 	$rootScope.$digest();
-	// 	$httpBackend.flush();
-	// 	expect( element.text() ).toEqual( 'Post type is posts' );
-	// } ) );
+	it( 'Text should be "Post type is posts"', inject( function( $rootScope, $compile ) {
+		var element = $compile( '<have-posts api-root="http://example.com" post-type="posts">Post type is {{ postType }}</div>' )( $rootScope );
+		$rootScope.$digest();
+		$httpBackend.flush();
+		expect( element.text() ).toEqual( 'Post type is posts' );
+	} ) );
 
-	// it( '<have-posts> should have class `posts`', inject( function( $rootScope, $compile ) {
-	// 	var element = $compile( '<have-posts api-root="http://example.com" post-type="posts" />' )( $rootScope );
-	// 	$rootScope.$digest();
-	// 	$httpBackend.flush();
-	// 	expect( element.hasClass( 'have-posts' ) ).toEqual( true );
-	// } ) );
+	it( '<have-posts> should have class `posts`', inject( function( $rootScope, $compile ) {
+		var element = $compile( '<have-posts api-root="http://example.com" post-type="posts" />' )( $rootScope );
+		$rootScope.$digest();
+		$httpBackend.flush();
+		expect( element.hasClass( 'have-posts' ) ).toEqual( true );
+	} ) );
 
-	// it( '<have-posts> should have class "pages"', inject( function( $rootScope, $compile ) {
-	// 	var element = $compile( '<have-posts api-root="http://example.com" post-type="pages" />' )( $rootScope );
-	// 	$rootScope.$digest();
-	// 	$httpBackend.flush();
-	// 	expect( element.hasClass( 'pages' ) ).toEqual( true );
-	// } ) );
+	it( '<have-posts> should have class "pages"', inject( function( $rootScope, $compile ) {
+		var element = $compile( '<have-posts api-root="http://example.com" post-type="pages" />' )( $rootScope );
+		$rootScope.$digest();
+		$httpBackend.flush();
+		expect( element.hasClass( 'pages' ) ).toEqual( true );
+	} ) );
 
-	// it( 'postType should be posts', inject( function( $rootScope, $compile ) {
-	// 	var element = $compile( '<have-posts api-root="http://example.com" post-type="posts" />' )( $rootScope );
-	// 	$rootScope.$digest();
-	// 	$httpBackend.flush();
-	// 	expect( $rootScope.postType ).toEqual( 'posts' );
-	// } ) );
+	it( 'postType should be posts', inject( function( $rootScope, $compile ) {
+		var element = $compile( '<have-posts api-root="http://example.com" post-type="posts" />' )( $rootScope );
+		$rootScope.$digest();
+		$httpBackend.flush();
+		expect( $rootScope.postType ).toEqual( 'posts' );
+	} ) );
 
-	// it( 'postId should be 123', inject( function( $rootScope, $compile ) {
-	// 	var element = $compile( '<have-posts api-root="http://example.com" post-type="posts" post-id="123" />' )( $rootScope );
-	// 	$rootScope.$digest();
-	// 	$httpBackend.flush();
-	// 	expect( $rootScope.query ).toEqual( { endpoint: 'posts', id: '123' } );
-	// 	expect( $rootScope.postId ).toEqual( '123' );
-	// } ) );
+	it( 'postId should be 123', inject( function( $rootScope, $compile ) {
+		var element = $compile( '<have-posts api-root="http://example.com" post-type="posts" post-id="123" />' )( $rootScope );
+		$rootScope.$digest();
+		$httpBackend.flush();
+		expect( $rootScope.query ).toEqual( { endpoint: 'posts', id: '123' } );
+		expect( $rootScope.postId ).toEqual( '123' );
+	} ) );
 } );
